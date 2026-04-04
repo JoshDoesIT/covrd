7791d9cbac378f268cf732c028458e6c8cb9f72d
Joshua Jones <devansmain@gmail.com>
2026-04-03 20:41:45 -0400

    chore: remove test script added by mistake

--- test_regex.js
+++
@@ -1,9 +0,0 @@
-const text = 'This is **bold** and *italic* text.'
-console.log(
-  'REGEX 1 (/\\*\\*(.*?)\\*\\*/g):',
-  text.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>'),
-)
-console.log(
-  'REGEX 2 (original code /\\\\*\\\\*(.*?)\\\\*\\\\*/g):',
-  text.replace(/\\*\\*(.*?)\\*\\*/g, '<strong>$1</strong>'),
-)
